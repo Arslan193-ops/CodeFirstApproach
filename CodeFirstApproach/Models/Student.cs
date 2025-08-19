d8cd7b4f30a10dcf84dcc6c892b46b36b873e882
Arslan <arslanmian604@gmail.com>
2025-08-19 16:38:27 +0500

    Add project files.

---
+++ CodeFirstApproach/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CodeFirstApproach.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Student Name",TypeName = "varchar(100)")]
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [Column("Student Gender", TypeName = "varchar(100)")]
+        public string Gender { get; set; }
+    }
+}
---
+++ CodeFirstApproach/Models/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions Options) :base(Options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
